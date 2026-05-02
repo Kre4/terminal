8197a8cfd73b67da7d64d87beb805e79fd4d8045
Kre4 <denhol2002@gmail.com>
2026-05-02 19:29:33 +0300

    feat: add ability to drag terminal window

--- composeApp/src/commonMain/kotlin/ru/kre4/App.kt
+++ composeApp/src/commonMain/kotlin/ru/kre4/App.kt
@@ -1,15 +1,20 @@
 package ru.kre4
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.kre4.terminal.theme.Terminal
 import ru.kre4.terminal.theme.TerminalTheme
+import ru.kre4.terminal.theme.animaton.SvgFallAnimation
 
 @Composable
 @Preview
@@ -22,6 +27,28 @@ fun App() {
             contentAlignment = Alignment.Center,
         ) {
             Terminal()
+    var windowOffsetX by remember { mutableStateOf(0f) }
+    var windowOffsetY by remember { mutableStateOf(0f) }
+
+    TerminalTheme {
+        Box() {
+            Box(
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .fillMaxSize()
+                    .offset(x = windowOffsetX.dp, y = windowOffsetY.dp)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            windowOffsetX += dragAmount.x
+                            windowOffsetY += dragAmount.y
+                        }
+                    },
+                contentAlignment = Alignment.Center,
+            ) {
+                Terminal()
+            }
+
         }
     }
 }
